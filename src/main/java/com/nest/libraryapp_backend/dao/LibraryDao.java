47a49cecaaaa038467339e1b92a127889aa1ed98
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-08 23:24:18 +0530

    added deletebook

--- src/main/java/com/nest/libraryapp_backend/dao/LibraryDao.java
+++ src/main/java/com/nest/libraryapp_backend/dao/LibraryDao.java
@@ -1,9 +1,11 @@
 package com.nest.libraryapp_backend.dao;
 
 import com.nest.libraryapp_backend.model.Library;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,4 +13,9 @@ public interface LibraryDao extends CrudRepository<Library,Integer> {
 
     @Query(value = "SELECT `id`, `author`, `description`, `img`, `price`, `title` FROM `library` WHERE `title`= :title",nativeQuery = true)
     List<Library>SearchBook(@Param("title")String title);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `library` WHERE `id`=:id",nativeQuery = true)
+    void DeleteBook(@Param("id")Integer id);
 }
