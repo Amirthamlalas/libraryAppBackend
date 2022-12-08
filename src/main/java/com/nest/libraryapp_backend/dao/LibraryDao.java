948765d97d66d40b510027005e4df27729d8cb33
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-08 23:12:29 +0530

    added booksearch

--- src/main/java/com/nest/libraryapp_backend/dao/LibraryDao.java
+++ src/main/java/com/nest/libraryapp_backend/dao/LibraryDao.java
@@ -1,7 +1,14 @@
 package com.nest.libraryapp_backend.dao;
 
 import com.nest.libraryapp_backend.model.Library;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LibraryDao extends CrudRepository<Library,Integer> {
+
+    @Query(value = "SELECT `id`, `author`, `description`, `img`, `price`, `title` FROM `library` WHERE `title`= :title",nativeQuery = true)
+    List<Library>SearchBook(@Param("title")String title);
 }
